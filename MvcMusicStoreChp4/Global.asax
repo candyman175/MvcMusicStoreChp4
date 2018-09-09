e80e6a1e2ffa83733c45bae8a11e1a67c02881af
David Wood <fourpointsservicesllc@gmail.com>
2018-09-09 16:49:05 -0500

    Add project files.

---
+++ MvcMusicStoreChp4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcMusicStoreChp4.MvcApplication" Language="C#" %>
---
+++ MvcMusicStoreChp4/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStoreChp4.Models
+{
+    public class Artist
+    {
+        public virtual int ArtistID { get; set; }
+        public virtual string Name{ get; set; }
+    }
+}
